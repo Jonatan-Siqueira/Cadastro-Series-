ab76f0eafa3c205ce65e59b4972bfe6ed0c2b832
Jonatan Siqueira <79473902+Jonatan-Siqueira@users.noreply.github.com>
2021-05-04 13:46:32 -0300

    Add files via upload

---
+++ CadastroSeries/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+
+namespace CadastroSeries
+{
+    class Program
+    {
+        enum Menu { ação = 1, Aventura, Romance, Anime, Dorama, Sair }
+        static void Main(string[] args)
+        {
+            bool Exit = false;
+            while (!Exit)
+            {
+                Console.WriteLine("================================");
+                Console.WriteLine("Catalogos de filmes");
+                Console.WriteLine("Gênero: ");
+
+                Console.WriteLine("1-Ação\n2-Aventura\n3-Romance\n4-Anime\n5-Dorama\n6-Sair");
+                Console.WriteLine("==========================");
+                string Op = Console.ReadLine();
+                int Opint = int.Parse(Op);
+                Console.WriteLine("==========================");
+
+                Menu Option = (Menu)Opint;
+
+                if (Opint > 0 && Opint < 6)
+                {
+                    switch (Option)
+                    {
+                        case Menu.ação:
+                            Acao();
+                            break;
+                        case Menu.Aventura:
+                            Aventura();
+                            break;
+                        case Menu.Romance:
+                            Romance();
+                            break;
+                        case Menu.Anime:
+                            Anime();
+                            break;
+                        case Menu.Dorama:
+                            Dorama();
+                            break;
+                        case Menu.Sair:
+                            Exit = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    Exit = true;
+                }
+                Console.Clear();
+            }
+        }
+        static void Acao()
+        {
+            Series serie = new Series();
+            serie.Acao();
+
+            Console.ReadLine();
+        }
+        static void Anime()
+        {
+            Series Serie = new Series();
+            Serie.Anime("Your Name", 2018, "UM jovem volta 300 anos no passado");
+
+            Console.ReadLine();
+        }
+        static void Aventura()
+        {
+            Series serie = new Series();
+            serie.Aventura("Maze Runner", 2017, "UM jovem preso em um Labirinto");
+
+            
+
+            Console.ReadLine();
+        }
+        static void Romance()
+        {
+            Series serie = new Series();
+            serie.Romance("A culpa é das Estrelas", 2015, "UMa jovem com Cancer Luta");
+
+            Console.ReadLine();
+        }
+        static void Dorama()
+        {
+            Series serie = new Series();
+            serie.Dorama("Uma Odisseia Coreana", 2016, "Son oh gong Enifim é libertado da prisão eterna, mas nem tudo é como ele imagina até ele conhecer janmgang");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CadastroSeries/Series.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroSeries
+{
+    class Series
+    {
+        public string Nome;
+        public int Ano;
+        public string Descrição;
+
+        public void Acao()
+        {
+            Nome = "Jump Sky";
+            Ano = 2019;
+            Descrição = "Um Homem Que Fará de Tudo para Salvar sua familia";
+
+            Console.WriteLine(">>>> Filme De Ação <<<<");
+            Console.WriteLine("=======================");
+
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Ano: {Ano}");
+            Console.WriteLine($"Descrição: {Descrição}");
+        }
+        public void Aventura(string Nome, int Ano, string Descrição)
+        {
+            Console.WriteLine(">>>> Filmes de Aventura <<<<");
+            Console.WriteLine("=======================");
+
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Ano: " + Ano);
+            Console.WriteLine("Descrição: " + Descrição);
+        }
+        public void Anime(string Nome, int Ano, string Descrição)
+        {
+            Console.WriteLine(">>>> Filmes de Anime <<<<");
+            Console.WriteLine("=======================");
+
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Ano: " + Ano);
+            Console.WriteLine("Descrição: " + Descrição);
+
+        }
+        public void Romance(string Nome, int Ano, string Descrição)
+        {
+            Console.WriteLine(">>>> Filmes de Romance <<<<");
+            Console.WriteLine("=======================");
+
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Ano: " + Ano);
+            Console.WriteLine("Descrição: " + Descrição);
+        }
+        public void Dorama(string Nome, int Ano, string Descrição)
+        {
+            Console.WriteLine(">>>> Dorama <<<<");
+            Console.WriteLine("=======================");
+
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Ano: " + Ano);
+            Console.WriteLine("Descrição: " + Descrição);
+        }
+    }
+
+}
